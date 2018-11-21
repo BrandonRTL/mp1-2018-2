a1a61452dd594e8f1e020c44f84017595d2ba472
BrandonRTL <43331560+BrandonRTL@users.noreply.github.com>
2018-11-21 22:11:05 +0300

    Поправил сортировку Хоара

--- kolesnikov_gr/task5/Source.c
+++ kolesnikov_gr/task5/Source.c
@@ -15,37 +15,41 @@ void Switch1(char str1[], char  str2[])
 	strncpy(str1, str2, 250);
 	strncpy(str2, addstr, 250);
 }
-void quick_Sort(unsigned long a[],int b[], int count) 
+void quick_Sort(unsigned long a[], int b[], int ll, int count)
 {
-	long i = 0, j = count - 1; 
-	long p = count / 2;
-	unsigned long temp; 
+	long i = ll, j = count;
+	long p = a[(count + ll) / 2];
+	unsigned long temp;
 	do {
-		while (a[i] < a[p]) i++;
-		while (a[j] > a[p]) j--;
-		if (i <= j) 
+		while (a[i] < p) i++;
+		while (a[j] > p) j--;
+		if (i <= j)
 		{
-			temp = a[i];
-			a[i] = a[j];
-			a[j] = temp;
-			temp = b[i];
-			b[i] = b[j];
-			b[j] = temp;
+			if (i < j)
+			{
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+				temp = b[i];
+				b[i] = b[j];
+				b[j] = temp;
+			}
 			i++;
 			j--;
 		}
 	} while (i <= j);
-	if (j > 0) quick_Sort(a,b, j);
-	if (count > i) quick_Sort(a + i,b, count - i);
+	if (j > ll) quick_Sort(a, b, ll, j);
+	if (count > i) quick_Sort(a, b, i, count);
 }
+
 int main()
 {
 	struct _finddata_t c_file;
 	intptr_t hFile;
-	clock_t t1, t2;
-	double T;
+	clock_t t1, t2, t3, tt;
 	char path[200];
 	int count = 0;
+	t3 = clock();
 	int k = 0, i = 0, j = 0, l = 0;
 	unsigned long sizez[100] = { 0 };
 	unsigned long x;
@@ -55,12 +59,14 @@ int main()
 	printf("Enter catalog adress with doubled slashes\n");
 	gets(path);
 	int a = strlen(path);
-	path[a + 1] = '\0';
-	path[a] = '*';
+	path[a + 3] = '\0';
+	path[a + 2] = '*';
+	path[a] = '\\';
+	path[a + 1] = '\\';
 	i = 0;
 	printf("Choose the sorting algorithm\n");
-	printf(" 1) - bubble sort\n 2) - select sort\n 3) - insert sort\n 4) - merge sort\n 5) - quick(Hoala) sort\n 6) - shell sort\n 7) - counting sort\n");
-	while ((k < 1) || (k > 7))
+	printf(" 1) - bubble sort\n 2) - select sort\n 3) - insert sort\n 4) - quick(Hoala) sort\n");
+	while ((k < 1) || (k > 4))
 		scanf("%d", &k);
 	if ((hFile = _findfirst(path, &c_file)) == -1L)
 		printf("No files fould!\n");
@@ -71,7 +77,7 @@ int main()
 		printf("----         ---- %24c   ----\n", ' ');
 		do {
 			char buffer[30];
-			if (i  > -1)
+			if (i > -1)
 			{
 				sizez[i] = c_file.size;
 				count++;
@@ -79,7 +85,7 @@ int main()
 			}
 			ctime_s(buffer, _countof(buffer), &c_file.time_write);
 			if (count < 20);
-				printf("%-12.12s %.24s  %10u\n", c_file.name, buffer, c_file.size);
+			printf("%-12.12s %.24s  %10u\n", c_file.name, buffer, c_file.size);
 			i++;
 		} while (_findnext(hFile, &c_file) == 0);
 		_findclose(hFile);
@@ -134,19 +140,19 @@ int main()
 		}
 		t2 = clock();
 		break;
-	case 5:
-		quick_Sort(sizez, addnamez, count);
-		break;
+	case 4:
+		quick_Sort(sizez, addnamez, 0, count - 1);
 		t2 = clock();
 		break;
 	}
-	T = (double)(t2 - t1)/(CLOCKS_PER_SEC);
-	printf("%f", T);
+	tt = t2 - t1;
+	printf("%16cFILE    SIZE\n", ' ');
 	for (i = 0; i < count; i++)
 	{
 		printf("%20.20s\t", namez[addnamez[i]]);
 		printf("%u\n", sizez[i]);
 	}
+	printf("Time of sorting: %u clocks\n", tt);
 	_getch();
 }
 
