8ae48af037dbe854f76a5dfd85140fe6f00bfefa
BrandonRTL <43331560+BrandonRTL@users.noreply.github.com>
2018-12-06 19:51:26 +0300

    Функции богу функций, указатели для трона из указателей!

--- kolesnikov_gr/task6/Source.c
+++ kolesnikov_gr/task6/Source.c
@@ -5,83 +5,114 @@
 #include <string.h>
 #include <time.h>
 #include <math.h>
-void Exponenta(double Tochnost, double x, int n)
+int ZnakChetn(int n) // + по четным; - по нечетным
 {
-	double ImExp = 1;
-	double ImAdding = 1;
+	int Znak;
+	if ((n % 2) == 0)
+		Znak = 1;
+	if ((n % 2) == 1)
+		Znak = -1;
+	return Znak;
+}
+int ZnakNeChetn(int n) // + по нечетным; - по четным
+{
+	int Znak;
+	if ((n % 2) == 0)
+		Znak = -1;
+	if ((n % 2) == 1)
+		Znak = 1;
+	return Znak;
+}
+
+long double Exponenta(double Tochnost, double x, int n)
+{
+	long double ImExp = 1;
+	long double ImAdding = 1;
 	int count = 0;
 	double y = exp(x);
-	for (int i = 1; i < n; i++)
+	for (int i = 2; i <= n; i++)
 	{
-		ImAdding = ImAdding * (x / i);
+		ImAdding = (ImAdding * x) / (i-1);
 		ImExp = ImExp + ImAdding;
 		count++;
-
-		if (abs(y - ImExp) < (Tochnost))
+		if (fabs(y - ImExp) < Tochnost)
 			i = n + 1;
-
 	}
-	printf("%lf\n", ImExp);
-	printf("%lf\n", y);
-	printf("%lf\n", ImExp - y);
-	printf("%d", count);
-
+	return ImExp;
 }
-void Arctangens(double Tochnost, double x, int n)
+long double Arctangens(double Tochnost, double x, int n)
 {
-	while (abs(x) > 1)
+	do
 	{
-		printf("|x|<=1");
+		printf("|x|<=1!!!!");
 		scanf_s("%f", &x);
+	} while (fabs(x) > 1);
+	long double ImArcTan = x;
+	long double ImAdding = x;
+	int i = 0;
+	int count = 0;
+	for (i = 2; i <= n; i++)
+	{
+		ImAdding = ImAdding * (-1)*((x * x)*(2*i-3)/(2*i-1));
+		ImArcTan = ImArcTan + ImAdding;
+		count++;
+		if (fabs(tan(x) - ImArcTan) < Tochnost)
+			i = n + 1;
 	}
+	return(ImArcTan);
 }
-void Cosinus (double Tochnost, double x, int n)
+long double Cosinus (double Tochnost, double x, int n)
 {
-
+	int i;
+	int count = 0;
+	long double ImCos = 1;
+	long double ImAdding = 1;
+	for (i = 2; i <= n; i++)
+	{
+		ImAdding = ImAdding*(-1)*((x * x) / ((2 * i - 2) * (2 * i - 3)));
+		ImCos = ImCos + ImAdding;
+		count++;
+		if (fabs(cos(x) - ImCos) < Tochnost)
+			i= n+1;
+	}
+	return ImCos;
 }
-void Sinus(double Tochnost, double x, int n)
+long double Sinus(double Tochnost, double x, int n)
 {
-
-}
-int ZnakChetn(int n)
-{
-	int Znak;
-	if ((n % 2) == 0)
-		Znak = 1;
-	if ((n % 2) == 1)
-		Znak = -1;
-	return Znak;
-}
-int ZnakNeChetn(int n)
-{
-	int Znak;
-	if ((n % 2) == 0)
-		Znak = -1;
-	if ((n % 2) == 1)
-		Znak = 1;
-	return Znak;
+	int i;
+	long double ImSin = x;
+	int count = 0;
+	long double ImAdding = x;
+	for (i = 2; i <= n; i++)
+	{
+		ImAdding = ImAdding*(-1)*((x * x) / ((2 * i - 2) * (2 * i - 1)));
+		ImSin = ImSin + ImAdding;
+		count++;
+		if (fabs(sin(x) - ImSin) < Tochnost)
+			i = n + 1;
+	}
+	return ImSin;
 }
 int main()
 {
 	double x, y, z, t;
 	int n = 100;
+	int j = 1;
 	int flag = 1;
 	double Tochnost = 1;
-	float ImAdding;
-	scanf_s("%lf", &x);
-	printf("%lf\n", x);
-	int count = 0;
-	int Znak;
-	x = 1; 
-	ImAdding = x * x*x;
-	float ImAtan = x;
-	y = atan(x);
-	printf("%lf\n", y);
-	for (int i = 1; i < 100; i++)
-	{
-		Znak = ZnakNeChetn(n);
-		ImAdding = ImAdding * x*x;
-		ImAtan = ImAtan + Znak*ImAdding/(2*i-1);
-	}
+	void(*p)();
+	long double(*operation)(long double, long double, int);
+	printf("Choose the function!\n");
+	printf("1 - sin\n2 - cos\n3 - exp\n4 - arctan");
+	scanf_s("%d", &j);
+	operation = Sinus;
+	if (j == 2)
+		operation = Cosinus;
+	if (j == 3)
+		operation = Exponenta;
+	if (j == 4)
+		operation = Arctangens;
+	y = operation(0.00001, 1.52, 100);
+	printf("%f", y);
 	_getch();
 }
