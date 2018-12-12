18c6e51f5af6786bbaab4f219027455fc6ccdcda
BrandonRTL <43331560+BrandonRTL@users.noreply.github.com>
2018-12-12 14:14:11 +0300

    исправил проверку значения х в арктангенсе

--- kolesnikov_gr/task6/Source.c
+++ kolesnikov_gr/task6/Source.c
@@ -5,26 +5,7 @@
 #include <string.h>
 #include <time.h>
 #include <math.h>
-int ZnakChetn(int n) // + по четным; - по нечетным
-{
-	int Znak;
-	if ((n % 2) == 0)
-		Znak = 1;
-	if ((n % 2) == 1)
-		Znak = -1;
-	return Znak;
-}
-int ZnakNeChetn(int n) // + по нечетным; - по четным
-{
-	int Znak;
-	if ((n % 2) == 0)
-		Znak = -1;
-	if ((n % 2) == 1)
-		Znak = 1;
-	return Znak;
-}
-
-long double Exponenta(double Tochnost, double x, int n)
+long double Exponenta(double Tochnost, double x, int n, int *Imcount) // MEHANIZM FUNKCII NA SVYAZI
 {
 	long double ImExp = 1;
 	long double ImAdding = 1;
@@ -38,30 +19,29 @@ long double Exponenta(double Tochnost, double x, int n)
 		if (fabs(y - ImExp) < Tochnost)
 			i = n + 1;
 	}
+	*Imcount = count+1;
 	return ImExp;
 }
-long double Arctangens(double Tochnost, double x, int n)
+long double Arctangens(double Tochnost, double x, int n, int *Imcount)
 {
-	do
-	{
-		printf("|x|<=1!!!!");
-		scanf_s("%lf", &x);
-	} while (fabs(x) > 1);
 	long double ImArcTan = x;
 	long double ImAdding = x;
 	int i = 0;
+	long double diff;
 	int count = 0;
 	for (i = 2; i <= n; i++)
 	{
 		ImAdding = ImAdding * (-1)*((x * x)*(2*i-3)/(2*i-1));
 		ImArcTan = ImArcTan + ImAdding;
 		count++;
-		if (fabs(tan(x) - ImArcTan) < Tochnost)
+		diff = fabs(tan(x) - ImArcTan);
+		if (diff < Tochnost)
 			i = n + 1;
 	}
+	*Imcount = count+1;
 	return(ImArcTan);
 }
-long double Cosinus (double Tochnost, double x, int n)
+long double Cosinus (double Tochnost, double x, int n, int *Imcount)
 {
 	int i;
 	int count = 0;
@@ -75,9 +55,10 @@ long double Cosinus (double Tochnost, double x, int n)
 		if (fabs(cos(x) - ImCos) < Tochnost)
 			i= n+1;
 	}
+	*Imcount = count+1;
 	return ImCos;
 }
-long double Sinus(double Tochnost, double x, int n)
+long double Sinus(double Tochnost, double x, int n, int *Imcount) // 4 FUNKCII
 {
 	int i;
 	long double ImSin = x;
@@ -91,40 +72,32 @@ long double Sinus(double Tochnost, double x, int n)
 		if (fabs(sin(x) - ImSin) < Tochnost)
 			i = n + 1;
 	}
+	*Imcount = count+1;
 	return ImSin;
 }
 int main()
 {
 	double x, y, z, t;
 	int n = 100;
-	x = 1.52;
 	long double ref;
 	int m;
 	int Nmax;
 	int* pm;
+	int i;
 	int j = 1;
 	int flag = 1;
 	int mode;
+	int count;
 	long double Tochnost = 1;
-	void(*p)();
-	long double(*operation)(long double, long double, int);
+	void(*p)();  // YKAZATEL NA FUNKCIYU ZDEC'
+	long double(*operation)(long double, long double, int, int*);  
 	printf("Choose game mode!\n1 - Single function calculation\n2 - Serial experiment\n");
 	scanf_s("%d", &mode);
-	if (mode == 2)
-	{
-		printf("Choose the number of experiments\n");
-		scanf_s("%d", &Nmax);
-	}
-	if (mode == 1)
-	{
-		printf("Choose the number of elements in a row!\n");
-		scanf_s("%d", &n);
-	}
-	printf("Choose x!\n");
-	scanf_s("%lf", &x);
 	printf("Choose the function!\n");
 	printf("1 - sin\n2 - cos\n3 - exp\n4 - arctan");
 	scanf_s("%d", &j);
+	printf("Choose x!\n");
+	scanf_s("%lf", &x);
 	operation = Sinus;
 	ref = sin(x);
 	if (j == 2)
@@ -139,14 +112,36 @@ int main()
 	}
 	if (j == 4)
 	{
+		while (fabs(x) > 1)
+		{
+			printf("|x|<=1!!!!");
+			scanf_s("%lf", &x);
+		}
 		operation = Arctangens;
 		ref = atan(x);
 	}
-	printf("Choose precision!\n");
-	scanf_s("%lf", &Tochnost);
-	y = operation(Tochnost, x, n);
-	printf("calculated value: %f\n", y);
-	printf("reference value: %lf\n", ref);
-	printf("difference: %lf\n", fabs(ref-y));
+	if (mode == 1)
+	{
+		printf("Choose the number of elements in a row!\n");
+		scanf_s("%d", &n);
+		printf("Choose precision!\n");
+		scanf_s("%lf", &Tochnost);
+		y = operation(Tochnost, x, n, &count);
+		printf("calculated value: %f\n", y);
+		printf("reference value: %lf\n", ref);
+		printf("difference: %lf\n", fabs(ref - y));
+		printf("number of elements: %d\n", count);
+	}
+	if (mode == 2)
+	{
+		printf("Choose the number of experiments!\n");
+		scanf_s("%d", &Nmax);
+		printf("N       Ref Value         Calc Value          Diff\n");
+		for (i = 1; i <= Nmax; i++)
+		{
+			y = operation(0, x, i, &count);
+			printf("%.2d %16.9lf    %14.9lf    %12.9lf\n", i, ref, y, fabs(ref - y));
+		}
+	}
 	_getch();
 }
